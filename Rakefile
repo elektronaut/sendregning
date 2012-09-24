f44266a08c82938fe703f12affe1597de0fc6ff7
Inge Jørgensen <inge@manualdesign.no>
2012-09-24 03:48:02 +0200

    Converted tabs to spaces

--- Rakefile
+++ Rakefile
@@ -2,43 +2,43 @@ require 'rubygems'
 require 'rake'
 
 begin
-	require 'jeweler'
-	Jeweler::Tasks.new do |gem|
-		gem.name = "sendregning"
-		gem.summary = "Ruby client for the SendRegning Web Service"
-		gem.description = "Ruby client for the SendRegning Web Service"
-		gem.email = "inge@manualdesign.no"
-		gem.homepage = "http://github.com/elektronaut/sendregning"
-		gem.authors = ["Inge Jørgensen"]
-		gem.add_dependency "httparty", ">= 0.6.1"
-		gem.add_dependency "builder", ">= 2.1.2"
-		gem.add_dependency "multipart-post", ">= 1.0.1"
-		#gem.add_development_dependency "thoughtbot-shoulda", ">= 0"
-		# gem is a Gem::Specification... see http://www.rubygems.org/read/chapter/20 for additional settings
-	end
-	Jeweler::GemcutterTasks.new
+  require 'jeweler'
+  Jeweler::Tasks.new do |gem|
+    gem.name = "sendregning"
+    gem.summary = "Ruby client for the SendRegning Web Service"
+    gem.description = "Ruby client for the SendRegning Web Service"
+    gem.email = "inge@manualdesign.no"
+    gem.homepage = "http://github.com/elektronaut/sendregning"
+    gem.authors = ["Inge Jørgensen"]
+    gem.add_dependency "httparty", ">= 0.6.1"
+    gem.add_dependency "builder", ">= 2.1.2"
+    gem.add_dependency "multipart-post", ">= 1.0.1"
+    #gem.add_development_dependency "thoughtbot-shoulda", ">= 0"
+    # gem is a Gem::Specification... see http://www.rubygems.org/read/chapter/20 for additional settings
+  end
+  Jeweler::GemcutterTasks.new
 rescue LoadError
-	puts "Jeweler (or a dependency) not available. Install it with: gem install jeweler"
+  puts "Jeweler (or a dependency) not available. Install it with: gem install jeweler"
 end
 
 require 'rake/testtask'
 Rake::TestTask.new(:test) do |test|
-	test.libs << 'lib' << 'test'
-	test.pattern = 'test/**/test_*.rb'
-	test.verbose = true
+  test.libs << 'lib' << 'test'
+  test.pattern = 'test/**/test_*.rb'
+  test.verbose = true
 end
 
 begin
-	require 'rcov/rcovtask'
-	Rcov::RcovTask.new do |test|
-		test.libs << 'test'
-		test.pattern = 'test/**/test_*.rb'
-		test.verbose = true
-	end
+  require 'rcov/rcovtask'
+  Rcov::RcovTask.new do |test|
+    test.libs << 'test'
+    test.pattern = 'test/**/test_*.rb'
+    test.verbose = true
+  end
 rescue LoadError
-	task :rcov do
-		abort "RCov is not available. In order to run rcov, you must: sudo gem install spicycode-rcov"
-	end
+  task :rcov do
+    abort "RCov is not available. In order to run rcov, you must: sudo gem install spicycode-rcov"
+  end
 end
 
 task :test => :check_dependencies
@@ -47,10 +47,10 @@ task :default => :test
 
 require 'rake/rdoctask'
 Rake::RDocTask.new do |rdoc|
-	version = File.exist?('VERSION') ? File.read('VERSION') : ""
+  version = File.exist?('VERSION') ? File.read('VERSION') : ""
 
-	rdoc.rdoc_dir = 'rdoc'
-	rdoc.title = "sendregning #{version}"
-	rdoc.rdoc_files.include('README*')
-	rdoc.rdoc_files.include('lib/**/*.rb')
+  rdoc.rdoc_dir = 'rdoc'
+  rdoc.title = "sendregning #{version}"
+  rdoc.rdoc_files.include('README*')
+  rdoc.rdoc_files.include('lib/**/*.rb')
 end
--- lib/sendregning/invoice.rb
+++ lib/sendregning/invoice.rb
@@ -1,147 +1,147 @@
 module Sendregning
 
-	class Invoice
+  class Invoice
 
-		OPTIONAL_ATTRIBUTES = [
-			# Request
-			:invoiceType, :creditedId, :orderNo, :invoiceDate, :orderNo,
-			:invoiceDate, :dueDate, :orderDate, :recipientNo, :address1, :address2,
-			:country, :email, :ourRef, :yourRef, :comment, :invoiceText, :printDunningInfo,
-			:itemTaxInclude,
+    OPTIONAL_ATTRIBUTES = [
+      # Request
+      :invoiceType, :creditedId, :orderNo, :invoiceDate, :orderNo,
+      :invoiceDate, :dueDate, :orderDate, :recipientNo, :address1, :address2,
+      :country, :email, :ourRef, :yourRef, :comment, :invoiceText, :printDunningInfo,
+      :itemTaxInclude,
 
-			# Response
-			:tax, :dueDate, :dunningFee, :invoiceNo, :total, :accountNo, :orgNrSuffix, :kid, :orgNo, :interestRate, :state
-		]
+      # Response
+      :tax, :dueDate, :dunningFee, :invoiceNo, :total, :accountNo, :orgNrSuffix, :kid, :orgNo, :interestRate, :state
+    ]
 
-		SHIPMENT_ATTRIBUTES = [
-			:shipment, :emailaddresses, :copyaddresses
-		]
-		
-		SHIPMENT_MODES = {
-			:paper           => 'PAPER',
-			:email           => 'EMAIL',
-			:paper_and_email => 'PAPER_AND_EMAIL'
-		}
+    SHIPMENT_ATTRIBUTES = [
+      :shipment, :emailaddresses, :copyaddresses
+    ]
+    
+    SHIPMENT_MODES = {
+      :paper           => 'PAPER',
+      :email           => 'EMAIL',
+      :paper_and_email => 'PAPER_AND_EMAIL'
+    }
 
-		attr_accessor :client
-		attr_accessor :name, :zip, :city
-		attr_accessor :optional, :shipment
-		attr_accessor :lines
+    attr_accessor :client
+    attr_accessor :name, :zip, :city
+    attr_accessor :optional, :shipment
+    attr_accessor :lines
 
-		def initialize(attributes={})
-			self.update(attributes)
-			@lines = []
-		end
-		
-		def update(attributes={})
-			@client  = attributes[:client] if attributes[:client]
-			@name    = attributes[:name]   if attributes[:name]
-			@zip     = attributes[:zip]    if attributes[:zip]
-			@city    = attributes[:city]   if attributes[:city]
-			self.optional = attributes
-			self.shipment = attributes
-		end
+    def initialize(attributes={})
+      self.update(attributes)
+      @lines = []
+    end
+    
+    def update(attributes={})
+      @client  = attributes[:client] if attributes[:client]
+      @name    = attributes[:name]   if attributes[:name]
+      @zip     = attributes[:zip]    if attributes[:zip]
+      @city    = attributes[:city]   if attributes[:city]
+      self.optional = attributes
+      self.shipment = attributes
+    end
 
-		def add_line(line)
-			line = Sendregning::Line.new(line) unless line.kind_of?(Sendregning::Line)
-			@lines << line
-			line
-		end
-		
-		# Sends an invoice
-		def send!
-			self.client.send_invoice(self)
-		end
-		
-		def paid?
-			state == 'paid'
-		end
+    def add_line(line)
+      line = Sendregning::Line.new(line) unless line.kind_of?(Sendregning::Line)
+      @lines << line
+      line
+    end
+    
+    # Sends an invoice
+    def send!
+      self.client.send_invoice(self)
+    end
+    
+    def paid?
+      state == 'paid'
+    end
 
-		# Renders invoice to XML
-		def to_xml(options={})
-			if options[:builder]
-				xml = options[:builder]
-			else
-				xml = Builder::XmlMarkup.new(:indent=>2)
-				xml.instruct! :xml, :version=>"1.0", :encoding=>"UTF-8"
-			end
-			xml.invoices do |invoices|
-				invoices.invoice do |invoice|
-					invoice.name @name
-					invoice.zip  @zip
-					invoice.city @city
-					
-					# Lines
-					if @lines.length > 0
-						invoice.lines do |line_builder|
-							@lines.each{|l| l.to_xml(:builder => line_builder)}
-						end
-					end
+    # Renders invoice to XML
+    def to_xml(options={})
+      if options[:builder]
+        xml = options[:builder]
+      else
+        xml = Builder::XmlMarkup.new(:indent=>2)
+        xml.instruct! :xml, :version=>"1.0", :encoding=>"UTF-8"
+      end
+      xml.invoices do |invoices|
+        invoices.invoice do |invoice|
+          invoice.name @name
+          invoice.zip  @zip
+          invoice.city @city
+          
+          # Lines
+          if @lines.length > 0
+            invoice.lines do |line_builder|
+              @lines.each{|l| l.to_xml(:builder => line_builder)}
+            end
+          end
 
-					# Optional attributes
-					if @optional.length > 0
-						invoice.optional do |optional|
-							@optional.each do |key, value|
-								key = key.to_sym
-								if value.kind_of?(Date) || value.kind_of?(Time)
-									value = value.strftime("%d.%m.%y")
-								end
-								optional.tag! key, value
-							end
-						end
-					end
+          # Optional attributes
+          if @optional.length > 0
+            invoice.optional do |optional|
+              @optional.each do |key, value|
+                key = key.to_sym
+                if value.kind_of?(Date) || value.kind_of?(Time)
+                  value = value.strftime("%d.%m.%y")
+                end
+                optional.tag! key, value
+              end
+            end
+          end
 
-					# Shipment attributes
-					if @shipment.length > 0
-						invoice.shipment do |shipment|
-							shipment_mode = (@shipment[:shipment] || :paper).to_sym
-							raise 'Invalid shipment mode!' unless SHIPMENT_MODES.keys.include?(shipment_mode)
-							shipment_mode = SHIPMENT_MODES[shipment_mode]
+          # Shipment attributes
+          if @shipment.length > 0
+            invoice.shipment do |shipment|
+              shipment_mode = (@shipment[:shipment] || :paper).to_sym
+              raise 'Invalid shipment mode!' unless SHIPMENT_MODES.keys.include?(shipment_mode)
+              shipment_mode = SHIPMENT_MODES[shipment_mode]
 
-							shipment.text! shipment_mode
-							@shipment.each do |key, values|
-								key = key.to_sym
-								unless key == :shipment
-									values = [values] unless values.kind_of?(Enumerable)
-									shipment.tag! key do |emails|
-										values.each{|v| emails.email v}
-									end
-								end
-							end
-						end
-					end
-				end
-			end
-		end
+              shipment.text! shipment_mode
+              @shipment.each do |key, values|
+                key = key.to_sym
+                unless key == :shipment
+                  values = [values] unless values.kind_of?(Enumerable)
+                  shipment.tag! key do |emails|
+                    values.each{|v| emails.email v}
+                  end
+                end
+              end
+            end
+          end
+        end
+      end
+    end
 
-		protected
-		
-			def optional=(attributes)
-				@optional ||= {}
-				attributes.each do |key, value|
-					@optional[key.to_sym] = value if OPTIONAL_ATTRIBUTES.include?(key.to_sym)
-				end
-				@optional
-			end
-		
-			def shipment=(attributes)
-				@shipment ||= {}
-				attributes.each do |key, value|
-					@shipment[key.to_sym] = value if SHIPMENT_ATTRIBUTES.include?(key.to_sym)
-				end
-				@shipment
-			end
-			
-			def method_missing(method, *args)
-				if OPTIONAL_ATTRIBUTES.include?(method)
-					@optional[method]
-				elsif SHIPPING_ATTRIBUTES.include?(method)
-					@shipping[method]
-				else
-					super
-				end
-			end
-		
-	end
+    protected
+    
+      def optional=(attributes)
+        @optional ||= {}
+        attributes.each do |key, value|
+          @optional[key.to_sym] = value if OPTIONAL_ATTRIBUTES.include?(key.to_sym)
+        end
+        @optional
+      end
+    
+      def shipment=(attributes)
+        @shipment ||= {}
+        attributes.each do |key, value|
+          @shipment[key.to_sym] = value if SHIPMENT_ATTRIBUTES.include?(key.to_sym)
+        end
+        @shipment
+      end
+      
+      def method_missing(method, *args)
+        if OPTIONAL_ATTRIBUTES.include?(method)
+          @optional[method]
+        elsif SHIPPING_ATTRIBUTES.include?(method)
+          @shipping[method]
+        else
+          super
+        end
+      end
+    
+  end
 
 end

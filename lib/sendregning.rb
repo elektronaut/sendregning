00ff63c5c55063718ec7885678fa92fd570f354c
Inge Jørgensen <inge@manualdesign.no>
2014-03-05 15:22:27 +0100

    Simplified gemspec, removed Jeweler

--- lib/sendregning.rb
+++ lib/sendregning.rb
@@ -8,6 +8,7 @@ dir = Pathname(__FILE__).dirname.expand_path
 require dir + 'sendregning/client'
 require dir + 'sendregning/invoice'
 require dir + 'sendregning/line'
+require dir + 'sendregning/version'
 
 module Sendregning
 end
---
+++ lib/sendregning/version.rb
@@ -0,0 +1,5 @@
+module Sendregning
+  unless Sendregning.const_defined?('VERSION')
+    VERSION = "0.1.2"
+  end
+end

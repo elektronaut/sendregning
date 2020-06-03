8419ce33c2ceffc88047d4af7e9d12cda63f775b
Inge Jørgensen <inge@anyone.no>
2020-06-03 15:26:58 +0200

    Version 0.2.2

--- lib/sendregning/version.rb
+++ lib/sendregning/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module Sendregning
-  VERSION = "0.2.1" unless Sendregning.const_defined?("VERSION")
+  VERSION = "0.2.2" unless Sendregning.const_defined?("VERSION")
 end
